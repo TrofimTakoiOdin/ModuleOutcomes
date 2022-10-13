7c8bcca80683b6540961f42d633cfadcdd592829
arseny <arseniy.trofimov1993@gmail.com>
2022-10-13 11:01:51 +0300

    Created a programme which solves the task

--- Program.cs
+++ Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string[] firstArray = new string[5] { "1212123", "233", ":')", "wOrlD", "Oops" };
+        string[] secondArray = new string[firstArray.Length];
+        ResultArray(firstArray, secondArray);
+        PrintArray(secondArray);
+    }
+    static void ResultArray(string[] arrayFirst, string[] array2)
+        {
+            int index = 0;
+            for (int i = 0; i < arrayFirst.Length; i++)
+            {
+                if (arrayFirst[i].Length <= 3)
+                {
+                    array2[index] = arrayFirst[i];
+                    index++;
+                }
+            }
+        }
+    static void PrintArray(string[] array)
+        {
+            
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write($"{array[i]} ");
+            }
+            Console.WriteLine();
+        }
+
+}
