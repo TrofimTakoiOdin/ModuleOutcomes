7cb38088508d38d2069d02c3bd5eeb69a7cbc448
arseny <arseniy.trofimov1993@gmail.com>
2022-10-13 14:58:19 +0300

    improved program

--- Program.cs
+++ Program.cs
@@ -2,20 +2,20 @@
 {
     private static void Main(string[] args)
     {
-        string[] firstArray = new string[5] { "1212123", "233", ":')", "wOrlD", "Oops" };
+        string[] firstArray = new string[5] { "1212123", "233", ":')", "rlD", "Oops" };
         string[] secondArray = new string[firstArray.Length];
         ResultArray(firstArray, secondArray);
         PrintArray(secondArray);
     }
     static void ResultArray(string[] arrayFirst, string[] arraySecond)
         {
-            int index = 0;
+            
             for (int i = 0; i < arrayFirst.Length; i++)
             {
                 if (arrayFirst[i].Length <= 3)
                 {
-                    arraySecond[index] = arrayFirst[i];
-                    index++;
+                    arraySecond[i] = arrayFirst[i];
+                    
                 }
             }
         }
